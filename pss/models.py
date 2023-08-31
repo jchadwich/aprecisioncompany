d222dac6e9f66d3f0901f19cb829114704ae8611
Alex Curley <acurley1431@gmail.com>
2023-08-31 15:23:42 -0400

    Added Contact to pss

--- pss/models.py
+++ pss/models.py
@@ -9,6 +9,20 @@ class Customer(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class Contact(models.Model):
+    """Contact information for an external person"""
+
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name="contacts"
+    )
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    extension = models.CharField(max_length=10, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Territory(models.Model):
     """Categorical geographic region/territory"""
 
