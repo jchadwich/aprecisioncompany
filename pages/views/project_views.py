34fd4e0ba73a05c4f198f79a8a9e9e1af4df7d17
Alex Curley <acurley1431@gmail.com>
2023-09-01 18:07:48 -0400

    Added dummy templates for project/survey instructions

--- pages/views/project_views.py
+++ pages/views/project_views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, FormView, ListView
 
 from pages.forms.projects import ProjectInstructionsForm, SurveyInstructionsForm
@@ -20,7 +21,17 @@ class SurveyInstructionsView(FormView):
     form_class = SurveyInstructionsForm
     template_name = "projects/survey_instructions.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project"] = get_object_or_404(Project, pk=self.kwargs["pk"])
+        return context
+
 
 class ProjectInstructionsView(FormView):
     form_class = ProjectInstructionsForm
     template_name = "projects/project_instructions.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project"] = get_object_or_404(Project, pk=self.kwargs["pk"])
+        return context
