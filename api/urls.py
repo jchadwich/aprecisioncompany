80efc934ca194830e12c9275a340d89b890563c6
Alex Curley <acurley1431@gmail.com>
2023-09-06 14:10:12 -0400

    Added contact list to the customer detail page

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,11 @@ router.register(
     "measurements", measurements.MeasurementViewSet, basename="measurements"
 )
 router.register(
+    "tables/contacts",
+    tables.ContactTableViewSet,
+    basename="tables-contacts",
+)
+router.register(
     "tables/customers", tables.CustomerTableViewSet, basename="tables-customers"
 )
 router.register(
